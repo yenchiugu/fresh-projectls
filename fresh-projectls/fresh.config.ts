121d77d2eaf7ea6efeb15b64ed4b4b65d1e719a3
Sam Ku <samku@kkcompany.com>
2024-09-25 00:06:43 +0800

    HTTPS

--- fresh-projectls/fresh.config.ts
+++ fresh-projectls/fresh.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 import kvOAuth from "./plugins/kv-oauth.ts";
 
+const keyPath = "./server.key";
+const certPath = "./server.crt";
+const key = await Deno.readTextFile(keyPath);
+const cert = await Deno.readTextFile(certPath);
+
 export default defineConfig({
+  key,
+  cert,
   plugins: [tailwind(), kvOAuth],
 });
